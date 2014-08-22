16d077356630860d769ff1bdd4e109ded9b6b700
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2014-08-22 14:39:42 +0200

    Version set to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jakub.wadolowski@cognifide.com'
 license          'Apache 2.0'
 description      'Installs/Configures cq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends          'java'
 depends          'ulimit'
