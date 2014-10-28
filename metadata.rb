49f917528283bd80f14b4403528d98081cd99a19
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2014-10-28 10:24:53 +0100

    chef-sugar updated to newset version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures cq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-depends          'chef-sugar', '= 2.3.0'
+depends          'chef-sugar', '~> 2.4.1'
 depends          'java', '= 1.28.0'
 depends          'ulimit', '= 0.3.2'
 depends          'cq-unix-toolkit', '= 1.2.0'
