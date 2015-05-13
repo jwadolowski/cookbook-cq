2b72aec104a77e2034efdfa2d29b329eee6a956c
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2015-05-13 15:53:20 +0200

    Initial test suite for factory configs

--- test/integration/osgi_config_create/serverspec/osgi_config_create_factory_spec.rb
+++ test/integration/osgi_config_create/serverspec/osgi_config_create_factory_spec.rb
@@ -1,1 +1,17 @@
 require_relative '../../../kitchen/data/spec_helper'
+
+describe 'Factory OSGi config com.example.random.factory' do
+  it 'there was NO attemtps to create it' do
+    expect(
+      @osgi_config_helper.log_entries(
+        'com.example.random.factory'
+      ).length
+    ).to eq(0)
+  end
+
+  it 'does NOT exist' do
+    expect(
+      @config_list.include?('com.example.random.factory')
+    ).to be false
+  end
+end
