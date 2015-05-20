9e8435448d76da09bd92210e8f8aa29b4c439be9
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2015-05-20 16:32:07 +0200

    Initial test suite for recently added factory OSGi config

--- test/integration/osgi_config_create/serverspec/osgi_config_create_factory_spec.rb
+++ test/integration/osgi_config_create/serverspec/osgi_config_create_factory_spec.rb
@@ -10,9 +10,42 @@ describe 'Factory OSGi config com.example.random.factory' do
     ).to eq(0)
   end
 
+  it 'there was a single check to verify factory PID presence' do
+    expect(
+      @osgi_config_helper.log_entries(
+        'com.example.random.factory',
+        'POST\ \/system\/console\/configMgr\/com\.example\.random\.factory\ '\
+        'HTTP\/1\.1'
+      ).length
+    ).to eq(1)
+  end
+
   it 'does NOT exist' do
     expect(
       @config_list.include?('com.example.random.factory')
     ).to be false
+  end
+end
+
+describe 'Factory OSGi com.adobe.granite.monitoring.impl.ScriptConfigImpl' do
+  it 'there was a single check to verify factory PID presence' do
+    expect(
+      @osgi_config_helper.log_entries(
+        'com.adobe.granite.monitoring.impl.ScriptConfigImpl',
+        'POST\ \/system\/console\/configMgr\/com\.adobe\.granite\.monitoring'\
+        '\.impl\.ScriptConfigImpl\ HTTP\/1\.1'
+      ).length
+    ).to eq(1)
+  end
+
+  it 'there was a single HTTP POST request that sets all the data' do
+    expect(
+      @osgi_config_helper.log_entries(
+        'com.adobe.granite.monitoring.impl.ScriptConfigImpl',
+        'POST\ \/system\/console\/configMgr\/%5BTemporary%20PID%20replaced'\
+        '%20by%20real%20PID%20upon%20save%5D\?.*factoryPid=com\.adobe\.'\
+        'granite\.monitoring\.impl\.ScriptConfigImpl.*\ HTTP\/1\.1'
+      ).length
+    ).to eq(1)
   end
 end
