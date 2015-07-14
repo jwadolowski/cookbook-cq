07c68a4780cb80e8d12b3f50823ad80f9f74c541
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2015-07-14 12:33:34 +0200

    User resource redefined

--- resources/user.rb
+++ resources/user.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: cq
 # Resource:: user
 #
-# Copyright (C) 2014 Jakub Wadolowski
+# Copyright (C) 2015 Jakub Wadolowski
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,20 +17,30 @@
 # limitations under the License.
 #
 
-actions :create, :remove, :modify, :manage
-default_action :create
+actions :modify
 
-attribute :login, :kind_of => String,
-                  :regex => /^([a-zA\-Z-_]+)/,
-                  :name_attribute => true,
-                  :required => true
-attribute :instance_mode, :equal_to => [:author, :publish],
-                          :required => true
-attribute :password, :kind_of => String,
-                     :default => nil
-attribute :first_name, :kind_of => String,
-                       :default => nil
-attribute :last_name, :kind_of => String,
-                      :default => nil
-attribute :email, :kind_of => String,
-                  :default => nil
+default_action :nothing
+
+attribute :id,                :kind_of => String, :name_attribute => true,
+                              :required => true
+attribute :username,          :kind_of => String, :required => true
+attribute :password,          :kind_of => String, :required => true
+attribute :instance,          :kind_of => String, :required => true
+attribute :email,             :kind_of => String, :required => false
+attribute :first_name,        :kind_of => String, :required => false
+attribute :last_name,         :kind_of => String, :required => false
+attribute :phone_number,      :kind_of => String, :required => false
+attribute :job_tile,          :kind_of => String, :required => false
+attribute :street,            :kind_of => String, :required => false
+attribute :mobile,            :kind_of => String, :required => false
+attribute :city,              :kind_of => String, :required => false
+attribute :postal_code,       :kind_of => String, :required => false
+attribute :country,           :kind_of => String, :required => false
+attribute :state,             :kind_of => String, :required => false
+attribute :title,             :kind_of => String, :required => false
+attribute :gender,            :kind_of => String, :required => false
+attribute :about,             :kind_of => String, :required => false
+attribute :enable,            :kind_of => [TrueClass, FalseClass],
+                              :required => false, :default => true
+attribute :user_password,     :kind_of => String, :required => false
+attribute :old_user_password, :kind_of => String, :required => false
