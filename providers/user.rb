03a4bb2c57e1f68de988ee9be6b20179e55d902b
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2015-07-14 15:51:59 +0200

    User path loading

--- providers/user.rb
+++ providers/user.rb
@@ -16,3 +16,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+def whyrun_supported?
+  true
+end
+
+def uri_parser(request_uri)
+  require 'uri'
+
+  begin
+    URI.parse(new_resource.instance + request_uri)
+  rescue => e
+    Chef::Application.fatal!("#{new_resource.instance} is not a valid URI!\n"\
+                             "Error description: #{e}")
+  end
+end
+
+def instance_get_request(request_uri)
+  require 'net/http'
+  require 'uri'
+
+  uri = uri_parser(request_uri)
+
+  http_request = Net::HTTP::Get.new(
+    uri.request_uri
+  ).basic_auth(new_resource.username, new_resource.password)
+
+  begin
+    Net::HTTP.new(uri.host, uri.port).request(http_request)
+  rescue => e
+    Chef::Application.fatal!("HTTP request failure: #{e}")
+  end
+end
+
+def user_path
+  require 'json'
+
+  request_uri = '/bin/querybuilder.json?'\
+    'path=/home/users&'\
+    '1_property=rep:authorizableId&'\
+    "1_property.value=#{new_resource.id}&"\
+    'p.limit=-1'
+
+  response = instance_get_request(request_uri)
+
+  json_response = JSON.parse(response.body)
+
+  Chef::Application.fatal!(
+    'Total number of returned records is not equal 1!'
+  ) if json_response['total'] != 1
+
+  json_response['hits'][0]['path']
+end
+
+def load_current_resource
+  @current_resource = Chef::Resource::CqUser.new(new_resource.name)
+
+  @current_resource.id(new_resource.id)
+
+  # Get user path
+  @user_path = user_path
+end
+
+action :modify do
+  # TODO
+end
