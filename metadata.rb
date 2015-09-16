5fc685e530f87517715d487c8c1d1ea84d73c2d1
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2015-09-16 15:54:13 +0200

    java cookbook updated to '~> 1.35.0'. CQ UNIX Toolkit dependecy constraint set to ~>

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures cq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-depends          'java',            '~> 1.31.0'
+depends          'java',            '~> 1.35.0'
 depends          'ulimit',          '~> 0.3.3'
-depends          'cq-unix-toolkit', '= 1.2.0'
+depends          'cq-unix-toolkit', '~> 1.2.0'
