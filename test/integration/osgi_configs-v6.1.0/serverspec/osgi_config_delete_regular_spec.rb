34b8750506bc1baceab06f9573903fb26b0d5073
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2016-10-25 23:24:04 +0200

    cq_osgi_config test suite for AEM 6.1.0 updated

--- test/integration/osgi_configs-v6.1.0/serverspec/osgi_config_delete_regular_spec.rb
+++ test/integration/osgi_configs-v6.1.0/serverspec/osgi_config_delete_regular_spec.rb
@@ -1,57 +1,34 @@
 require_relative '../../../kitchen/data/spec_helper'
 
 describe 'OSGi com.adobe.cq.wcm.launches.impl.LaunchesEventHandler' do
-  it 'exists' do
-    expect(
-      @config_list.include?(
-        'com.adobe.cq.wcm.launches.impl.LaunchesEventHandler'
-      )
-    ).to be true
-  end
-
-  it 'there was 1 UPDATE request' do
-    expect(
-      @osgi_config_helper.regular_update_requests(
-        'com.adobe.cq.wcm.launches.impl.LaunchesEventHandler'
-      ).length
-    ).to eq(1)
-  end
-
-  it 'there was 0 DELETE requests' do
-    expect(
-      @osgi_config_helper.delete_requests(
-        'com.adobe.cq.wcm.launches.impl.LaunchesEventHandler'
-      ).length
-    ).to eq(0)
-  end
-
   # 1) Read current
   # 2) Update
   # 3) Read current
-  it 'in total there were 3 HTTP requests' do
+  # 4) Delete
+  it 'in total there were 4 HTTP requests' do
     expect(
       @osgi_config_helper.all_requests(
         'com.adobe.cq.wcm.launches.impl.LaunchesEventHandler'
       ).length
-    ).to eq(3)
+    ).to eq(4)
   end
 
-  it 'launches.eventhandler.threadpool.maxsize is set to 100' do
+  it 'launches.eventhandler.threadpool.maxsize is set to 5' do
     expect(
       @osgi_config_helper.config_value(
         'com.adobe.cq.wcm.launches.impl.LaunchesEventHandler',
         'launches.eventhandler.threadpool.maxsize'
       )
-    ).to eq(100)
+    ).to match(/^5$/)
   end
 
-  it 'launches.eventhandler.threadpool.priority is set to MAX' do
+  it 'launches.eventhandler.threadpool.priority is set to MIN' do
     expect(
       @osgi_config_helper.config_value(
         'com.adobe.cq.wcm.launches.impl.LaunchesEventHandler',
         'launches.eventhandler.threadpool.priority'
       )
-    ).to match(/^MAX$/)
+    ).to match(/^MIN$/)
   end
 
   it 'launches.eventhandler.updatelastmodification is set to false' do
@@ -65,14 +42,6 @@ describe 'OSGi com.adobe.cq.wcm.launches.impl.LaunchesEventHandler' do
 end
 
 describe 'OSGi com.adobe.cq.commerce.impl.promotion.PromotionManagerImpl' do
-  it 'there was 0 DELETE requests' do
-    expect(
-      @osgi_config_helper.delete_requests(
-        'com.adobe.cq.commerce.impl.promotion.PromotionManagerImpl'
-      ).length
-    ).to eq(0)
-  end
-
   # Just a READ request
   it 'in total there was 1 HTTP request' do
     expect(
@@ -93,14 +62,6 @@ describe 'OSGi com.adobe.cq.commerce.impl.promotion.PromotionManagerImpl' do
 end
 
 describe 'OSGi com.adobe.granite.auth.oauth.impl.TwitterProviderImpl' do
-  it 'there was 1 DELETE request' do
-    expect(
-      @osgi_config_helper.delete_requests(
-        'com.adobe.granite.auth.oauth.impl.TwitterProviderImpl'
-      ).length
-    ).to eq(1)
-  end
-
   # READ + DELETE
   it 'in total there were 2 HTTP requests' do
     expect(
