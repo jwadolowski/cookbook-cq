34d4ae6603f969309d6d9df7b69c45abf5c9681d
Jakub Wadolowski <jakub.wadolowski@cognifide.com>
2016-11-04 01:14:23 +0100

    Default sleep_time reduced to 3s for all cq_osgi_* resources

--- libraries/resource_cq_osgi_bundle.rb
+++ libraries/resource_cq_osgi_bundle.rb
@@ -40,7 +40,7 @@ class Chef
         @same_state_barrier = 3
         @error_state_barrier = 3
         @max_attempts = 30
-        @sleep_time = 5
+        @sleep_time = 3
       end
 
       def symbolic_name(arg = nil)
