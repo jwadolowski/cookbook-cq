7e33ac41eaf29eaa0a9a6c9e7e14563235a885b5
Vincent V <vincent.v@unknown.com>
2017-11-23 11:39:54 +0100

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jakub.wadolowski@cognifide.com'
 license          'Apache 2.0'
 description      'Installs/Configures cq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends          'java',            '~> 1.50.0'
 depends          'ulimit',          '~> 0.3.3'
