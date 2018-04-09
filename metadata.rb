ca2ce44d09d91893a54b09aa01f643fe2319fdbc
jwadolowski <jakub.wadolowski@cognifide.com>
2018-04-09 11:18:04 +0200

    Description update

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'cq'
 maintainer       'Jakub Wadolowski'
 maintainer_email 'jakub.wadolowski@cognifide.com'
 license          'Apache-2.0'
-description      'Installs/Configures cq'
+description      'Installs and configures Adobe AEM (formerly CQ)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
