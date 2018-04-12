84ede4a9ee36a86060aa6d26ce9d9d2af8a44090
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2018-04-12 17:26:10 +0200

    Default JDK version changed to 8

--- attributes/default.rb
+++ attributes/default.rb
@@ -55,6 +55,6 @@ default['cq']['crypto']['log_libs']['data'] = {
 
 # Java attributes
 # -----------------------------------------------------------------------------
-default['java']['jdk_version'] = '7'
+default['java']['jdk_version'] = '8'
 default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
